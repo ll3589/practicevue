72216d5b3fb751b211784c4f5eff20961f88fdca
ll3589 <ll3589@tc.columbia.edu>
2023-01-04 01:28:56 -0500

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Sign Up</h1>
+    <img src="../assets/cactus.png" alt="logo of signup" class="logo">
+    <div class="register">
+        <input type="text" placeholder="Your Name">
+        <br>
+        <input type="text" placeholder="Your Email">
+        <button>Sign Up</button>
+    </div>
+</template>
+<script>
+export default{
+    name:'SignUp'
+}
+</script>
+<style>
+.logo{
+    width:200px;
+    height: 100px;
+};
+.register{
+    width: 300px;
+    height: 40px;
+    padding: 20px;
+    display: block;
+}
+</style>
